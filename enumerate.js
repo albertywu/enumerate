e76f2b782ffdf673cfecad44a767c031b3cd37c9
Albert Wu <albertywu@gmail.com>
2016-09-01 07:10:57 -0700

    namespace and clean up tests

--- enumerate.js
+++ enumerate.js
@@ -1,5 +1,5 @@
 // (words: string[], cutoff: number, placeholder: string) => string
-const enumerate = (words, cutoff = 4, placeholder) => {
+const enumerate = (words, cutoff = 4, placeholder = 'Things') => {
   if (words.length === 0) return ''
   else if (words.length === 1) return words[0]
   else if (words.length >= cutoff) return `${words.length} ${placeholder}`
--- test/enumerate.spec.js
+++ test/enumerate.spec.js
@@ -1,36 +1,60 @@
 const enumerate = require('../enumerate')
-
 const expect = require('expect')
-
-const runTests = (fn, tests) => tests.forEach(([args, output]) => expect(fn.apply(this, args)).toBe(output))
 
 describe('enumerate', function() {
 
-  it('should pass all tests', function() {
-    const tests = [
-      [
-        [[], 4, 'People'],
-        ''
-      ],
-      [
-        [['Rick'], 4, 'People'],
-        'Rick'
-      ],
-      [
-        [['Rick', 'Boris'], 4, 'People'],
-        'Rick and Boris'
-      ],
-      [
-        [['Rick', 'Boris', 'Albert'], 4, 'People'],
-        'Rick, Boris, and Albert'
-      ],
-      [
-        [['Rick', 'Boris', 'Albert', 'Parag'], 4, 'People'],
-        '4 People'
-      ]
-    ]
+  describe('defaults', function() {
+    it('should return an empty string, when words is length 0', function() {
+      expect(
+        enumerate([])
+      ).toBe('')
+    })
 
-    runTests(enumerate, tests)
+    it('should return "X", when words is length 1', function() {
+      expect(
+        enumerate(['Screwdriver'])
+      ).toBe('Screwdriver')
+    })
+
+    it('should return "X and Y" when words is length 2', function() {
+      expect(
+        enumerate(['Screwdriver', 'Hammer'])
+      ).toBe('Screwdriver and Hammer')
+    })
+
+    it('should return "X, Y, and Z" when words is length 3', function() {
+      expect(
+        enumerate(['Screwdriver', 'Hammer', 'Drill'])
+      ).toBe('Screwdriver, Hammer, and Drill')
+    })
+
+    it('should return "4 Things" when words is length 4', function() {
+      expect(
+        enumerate(['Screwdriver', 'Hammer', 'Drill', 'Saw'])
+      ).toBe('4 Things')
+    })
+  })
+
+  describe('cutoff option', function() {
+    it('should cut off of at 3, when words is length 3 and cutoff is 3', function() {
+      expect(
+        enumerate(['Screwdriver', 'Hammer', 'Drill'], 3)
+      ).toBe('3 Things')
+    })
+
+    it('should not cut off of at 2, when words is length 2 and cutoff is 3', function() {
+      expect(
+        enumerate(['Screwdriver', 'Hammer'], 3)
+      ).toBe('Screwdriver and Hammer')
+    })
+  })
+
+  describe('placeholder option', function() {
+    it('should use a difference placeholder word, when placeholder is specified and cutoff is reached', function() {
+      expect(
+        enumerate(['Screwdriver', 'Hammer', 'Drill'], 3, 'Thingamabobs')
+      ).toBe('3 Thingamabobs')
+    })
   })
 
 })

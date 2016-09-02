fb45928a4f752264aa7f17816a4c1844cf0a3f57
Albert Wu <albertywu@gmail.com>
2016-09-01 21:05:04 -0700

    rm .vscode folder and add to .gitignore

--- enumerate.d.ts
+++ enumerate.d.ts
@@ -1,4 +1,1 @@
 export declare const enumerate: (words: string[], cutoff?: number, placeholder?: string) => string;
-export declare const foo: number;
-export declare const boo: number;
-export declare const roo: number;
--- enumerate.es6.js
+++ enumerate.es6.js
@@ -1,4 +1,3 @@
-// (words: string[], cutoff: number, placeholder: string) => string
 export const enumerate = (words, cutoff = 4, placeholder = 'Things') => {
     if (words.length === 0)
         return '';
@@ -9,6 +8,3 @@ export const enumerate = (words, cutoff = 4, placeholder = 'Things') => {
     else
         return `${words.slice(0, words.length - 1).join(', ')}${words.length === 2 ? '' : ','} and ${[...words].pop()}`;
 };
-export const foo = 43;
-export const boo = 43;
-export const roo = 555;

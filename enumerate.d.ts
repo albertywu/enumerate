228470687fd309b9243799c57772b6fc50a32156
Albert Wu <albertywu@gmail.com>
2016-09-02 13:31:38 -0700

    rename

--- enumerate.d.ts
+++ enumerate.d.ts
@@ -1,8 +1,8 @@
-export interface Args {
+export interface EnumerateOptions {
     words: string[];
     cutoff?: number;
     placeholder?: string;
     andStr?: string;
     noOxford?: boolean;
 }
-export declare function enumerate({words, cutoff, placeholder, andStr, noOxford}: Args): string;
+export declare function enumerate({words, cutoff, placeholder, andStr, noOxford}: EnumerateOptions): string;

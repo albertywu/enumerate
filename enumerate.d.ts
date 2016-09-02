ab11808de61648c084e9351d847021ec0e033041
Vinh Tran <vinhtran@gmail.com>
2016-09-01 21:18:19 -0700

    Implementing code review changes

--- enumerate.d.ts
+++ enumerate.d.ts
@@ -1,1 +1,1 @@
-export declare const enumerate: (words: string[], cutoff: number, placeholder: string, andStr: string, noOxford: any) => string;
+export declare const enumerate: (words: string[], cutoff?: number, placeholder?: string, andStr?: string, noOxford?: boolean) => string;
--- enumerate.ts
+++ enumerate.ts
@@ -1,4 +1,4 @@
-export const enumerate = (words: string[], cutoff = 4, placeholder = 'Things', andStr = 'and', noOxford): string => {
+export const enumerate = (words: string[], cutoff = 4, placeholder = 'Things', andStr = 'and', noOxford?: boolean): string => {
   if (words.length === 0) return ''
   else if (words.length === 1) return words[0]
   else if (words.length >= cutoff) return `${words.length} ${placeholder}`
